c42ae66dec0c00d2121d56816699c32be114f20e
Sattarkha <156591821+Sattarkha@users.noreply.github.com>
2024-02-29 16:14:38 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,77 @@
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("sattar");
+// let personName = "sattar";
+// console.log(personName);
+
+// let message: string="hello chacho";
+// message="sattar";
+// console.log(message);
+
+// let message- = "fankar";
+// console.log(message-)
+
+
+// let $isMarried = "sattar";
+
+// let person_Name = "abdul sattar";
+// let age = 34;
+// let unmarried = false;
+// console.log(person_Name);
+// console.log(age);
+// console.log(unmarried);
+
+
+// let person_Name="sattar";
+// let age=23;
+// let unmarried=false;
+
+// console.log(person_Name,age,unmarried);
+
+
+// let names: string[]=["ahsen","hassan","junaid","shehzad"];
+// let message: string="how are you dear";
+
+// console.log(names[0],message);
+
+// let num1 = "1";
+// let num2 = "2";
+// console.log(num1 + num2);
+
+// let num1 = 1;
+// let num2 = 1;
+// console.log(num1 + num2)
+
+
+// let n=5+5;
+// console.log(n);
+// let n=10-5;
+// console.log(n);
+
+// let n = 5;
+// console.log(n==5);
+// console.log(n===5);
+// console.log(n !==5);
+// console.log(n>8);
+// console.log(n<8);
+// console.log(n>=8);
+// console.log(n<=8);
+
+let myName = "sattar";
+let hello = "hello" + myName;
+console.log(hello);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
